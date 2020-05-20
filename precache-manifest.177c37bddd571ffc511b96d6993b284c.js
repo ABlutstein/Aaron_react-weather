497642057d392939eefa6c702cf23a4d0419cc3c
Work H <aaronblutstein@Work-H.local>
2020-05-20 11:30:30 +0100

    Updates

---
+++ precache-manifest.177c37bddd571ffc511b96d6993b284c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03fd3b23935d968e5aebbd1885578ee4",
+    "url": "/aaron_react-weather/index.html"
+  },
+  {
+    "revision": "6349eb0c5229478cb6a0",
+    "url": "/aaron_react-weather/static/css/main.456f872d.chunk.css"
+  },
+  {
+    "revision": "c6965a63410781e0cd17",
+    "url": "/aaron_react-weather/static/js/2.8fdcb456.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aaron_react-weather/static/js/2.8fdcb456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6349eb0c5229478cb6a0",
+    "url": "/aaron_react-weather/static/js/main.445615a7.chunk.js"
+  },
+  {
+    "revision": "5b9f00c2d7e922970646",
+    "url": "/aaron_react-weather/static/js/runtime-main.d9ced387.js"
+  },
+  {
+    "revision": "9849d3e279106d5f6607ae66a9b8a599",
+    "url": "/aaron_react-weather/static/media/weatherpic.9849d3e2.jpg"
+  }
+]);
